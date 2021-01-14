47e34f7a9084ab8318a4081902409021b4716dd3
Valeria <valery0007@gmail.com>
2021-01-14 17:46:25 +0100

    fixing errors

--- exercise/app.js
+++ exercise/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const port = 3000;
-//const axios = require('axios');
+const axios = require('axios');
 const Twitter = require('./api/helpers/twitter');
 const twitter = new Twitter();
 require('dotenv').config();
@@ -10,16 +10,17 @@ app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     next();
 });
+app.use(express.json());
 
 app.get('/tweets', (req, res) => {
     //console.log(req.query);
     const query = req.query.q;
-    const count = req.query.count;
+    // const count = req.query.count;
+    const resultType = req.query.result_type;
     const maxId = req.query.max_id;
     //  console.log(process.env.TWITTER_API_TOKEN);
 
-
-    twitter.get(query, count, maxId).then((response) => {
+    twitter.get(query, resultType, maxId).then((response) => {
         res.status(200).send(response.data);
     }).catch((error) => {
         res.status(400).send(error);
